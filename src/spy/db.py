f323132ecf5f77f87671753ec09aa00302d926be
badrpas <dezivert@gmail.com>
2015-05-23 11:24:41 +0300

    get_users

--- src/spy/db.py
+++ src/spy/db.py
@@ -68,9 +68,9 @@ def add_online_status(user_id, online, date):
             print(e._full_msg)
 
 def get_users():
-    query = (   "select *"
-                "from online_history as a"
-                "where a.id = (select max(id) from online_history where user_id = a.user_id group by user_id)"
+    query = (   "select * "
+                "from online_history as a "
+                "where a.id = (select max(id) from online_history where user_id = a.user_id group by user_id) "
                 "group by user_id;")
 
     cursor.execute(query)
