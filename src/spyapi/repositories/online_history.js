43fdf3ad76e9bc445e5a00568d2de3a607b7ecbb
badrpas <dezivert@gmail.com>
2015-05-24 17:07:35 +0300

    API methods created

---
+++ src/spyapi/repositories/online_history.js
@@ -0,0 +1,47 @@
+var mysql = require('mysql');
+
+var connectionConfig = require('./connection_config.json');
+
+var connection = mysql.createConnection(connectionConfig);
+var repository = function (){
+
+    this.getHistory = function (id, callback) {
+        connection.query('select * from online_history where user_id = ?;', [id], function (err, rows, fields) {
+            if (err) throw err;
+            console.log(rows);
+            callback(rows);
+        });
+    },
+
+    this.getSchedule = function (id, callback) {
+        this.getHistory(id, function(rows) {
+            var onlineTimes = [];
+            var start, end;
+            var idCounter = 0;
+
+            for ( var i = 0; i < rows.length; i++) {
+                if( rows[i].online_status_change == 1) {
+                    start = rows[i];
+                    end = null;
+                } else {
+                    end = rows[i];
+                }
+                if (start && end) {
+                    onlineTimes.push( {
+                        id: idCounter++,
+                        start: start.ondate,
+                        end: end.ondate
+                    });
+                    start = end = null;
+                }
+            }
+            console.log(onlineTimes);
+            callback(onlineTimes);
+        });
+    }
+
+};
+
+
+
+module.exports = new repository();
--- src/spyapi/routes/api.js
+++ src/spyapi/routes/api.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var repository = require('../repositories/online_history')
+
 /* GET users listing. */
+router.get('/history/:id', function(req, res, next) {
+    var rows = repository.getHistory(req.params.id, function(rows){
+        res.send(rows);
+    });
+});
 router.get('/schedule/:id', function(req, res, next) {
-    res.send(req.params);
+    var rows = repository.getSchedule(req.params.id, function(onlines){
+        res.send(onlines);
+    });
 });
 
 module.exports = router;
